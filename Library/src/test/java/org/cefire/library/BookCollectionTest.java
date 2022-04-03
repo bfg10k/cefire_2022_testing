1e0a06bf6c127ac22fc655f9d97b2943bbfd439c
marcos <marcosdlfp@gmail.com>
2022-04-03 17:16:55 +0200

    soluciones

--- Library/src/test/java/org/cefire/library/BookCollectionTest.java
+++ Library/src/test/java/org/cefire/library/BookCollectionTest.java
@@ -13,15 +13,16 @@ public class BookCollectionTest {
 
         BookCollection books = new BookCollection(new Book[]{
                 new Book("un-isbn-1", "un titulo 1", "un autor 1"),
-                new Book(isbnToLocate, "un titulo 2", "un autor 2"),
+                new Book(isbnToLocate, "un titulo 5", "un autor 4"),
+                new Book(isbnToLocate, "un titulo 6", "un autor 5"),
                 new Book(isbnToLocate, "un titulo 2", "un autor 2"),
                 new Book("un-isbn-3", "un titulo 3", "un autor 3"),
         });
 
         List<Book> foundBooks = books.find(isbnToLocate);
 
-        Assertions.assertFalse(foundBooks::isEmpty);
 
+        Assertions.assertEquals(3, foundBooks.size());
         foundBooks.forEach((Book book)-> Assertions.assertEquals(isbnToLocate, book.getISBN()));
     }
 
@@ -35,5 +36,31 @@ public class BookCollectionTest {
         });
 
         Assertions.assertTrue(books.find(isbnToLocate).isEmpty());
+    }
+
+    @Test
+    public void shouldFindBooksByPartialTitle(){
+        final String partialTitleToLocate = "titulo-a-encontrar";
+
+        BookCollection books = new BookCollection(new Book[]{
+           new Book("un-isbn-cualquiera", partialTitleToLocate, "un-autor-cualquiera"),
+           new Book("un-isbn-cualquiera-2", partialTitleToLocate.concat("-en-la-colección"), "un-autor-cualquiera"),
+           new Book("un-isbn-cualquiera-44543", "un-titulo-no-coincidente", "un-autor-cualquiera"),
+           new Book("un-isbn-cualquiera-423423", "otro-titulo-no-coincidente", "un-autor-cualquiera"),
+        });
+
+        List<Book> foundBooks = books.find(partialTitleToLocate);
+        Assertions.assertEquals(2, foundBooks.size());
+        foundBooks.forEach((Book book) -> Assertions.assertTrue(book.getTitle().contains(partialTitleToLocate)));
+    }
+
+    @Test
+    public void shoudlNotFindCoincidencesIfCollectionIsEmpty(){
+        final String isbnOrTitleThatShouldNotBeFound = "";
+        BookCollection books = new BookCollection(new Book[]{});
+
+        List<Book> foundBooks = books.find(isbnOrTitleThatShouldNotBeFound);
+
+        Assertions.assertTrue(foundBooks::isEmpty);
     }
 }
